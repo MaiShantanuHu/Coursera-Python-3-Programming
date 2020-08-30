ff76d7a63d73ca8c2dba287da665afedd8cef7c7
shantanu <66246470+MaiShantanuHu@users.noreply.github.com>
2020-08-31 00:13:06 +0530

    Update Dictionary Mechanics.py

--- Python Functions, Files, and Dictionaries/Dictionary Mechanics.py
+++ Python Functions, Files, and Dictionaries/Dictionary Mechanics.py
@@ -52,6 +52,12 @@ total_golds = {"Italy": 114, "Germany": 782, "Pakistan": 10, "Sweden": 627, "USA
 chile_golds = total_golds.get("Chile")
 print(chile_golds)
 
+''' Q-8. Provided is a dictionary called US_medals which has the first 70 metals that the United States has won in 2016, and in which category they have won it in. Using dictionary mechanics, assign the value of the key
+"Fencing" to a variable fencing_value. Remember, do not hard code this.'''
+
+US_medals = {"Swimming": 33, "Gymnastics": 6, "Track & Field": 6, "Tennis": 3, "Judo": 2, "Rowing": 2, "Shooting": 3, "Cycling - Road": 1, "Fencing": 4, "Diving": 2, "Archery": 2, "Cycling - Track": 1, "Equestrian": 2, "Golf": 1, "Weightlifting": 1}
+fencing_value = US_medals.get("Fencing")
+print(fencing_value)
 
 
 
