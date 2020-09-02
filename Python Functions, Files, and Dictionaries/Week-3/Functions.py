166f94e6487ca25ec3bec19fc113d56f03bde89e
shantanu <66246470+MaiShantanuHu@users.noreply.github.com>
2020-09-02 22:48:37 +0530

    Add files via upload

---
+++ Python Functions, Files, and Dictionaries/Week-3/Functions.py
@@ -0,0 +1,19 @@
+''' Q-1. Write a function called int_return that takes an integer as input and returns the same integer.'''
+
+def int_return(x):
+    return x
+
+int_return(3)
+    
+''' Q-2. Write a function called add that takes any number as its input and returns that sum with 2 added.'''
+
+def add(y):
+    z = 2 + y
+    return z
+
+r = add(2)
+print(r)
+
+''' Q-3.     
+
+
