65bf9d94504c3a8d38d626b6514f0df656d4ece3
shantanu <66246470+MaiShantanuHu@users.noreply.github.com>
2020-09-02 22:50:05 +0530

    Update Functions.py

--- Python Functions, Files, and Dictionaries/Week-3/Functions.py
+++ Python Functions, Files, and Dictionaries/Week-3/Functions.py
@@ -14,6 +14,12 @@ def add(y):
 r = add(2)
 print(r)
 
-''' Q-3.     
+''' Q-3. Write a function called change that takes any string, adds “Nice to meet you!” to the end of the argument given, and returns that new string.'''
+
+def change(x):
+    z = x + "Nice to meet you!"
+    return z
+r = change("hii bro")
+print(r)
 
 
