d9d22a21b3bee054beb798339c99c3959f5031b2
shantanu <66246470+MaiShantanuHu@users.noreply.github.com>
2020-09-06 23:08:32 +0530

    Update Advance functions.py

--- Python Functions, Files, and Dictionaries/Week-4/Advance functions.py
+++ Python Functions, Files, and Dictionaries/Week-4/Advance functions.py
@@ -52,3 +52,20 @@ def checkingIfIn(a, direction = True, d = {'apple': 2, 'pear': 1, 'fruit': 19, '
         else:
             return False
 
+'''Q-6 We have provided the function checkingIfIn such that if the first input parameter is in the third, dictionary, input parameter, then the function returns that value, and otherwise, it returns False.
+Follow the instructions in the active code window for specific variable assignmemts.'''
+
+def checkingIfIn(a, direction = True, d = {'apple': 2, 'pear': 1, 'fruit': 19, 'orange': 5, 'banana': 3, 'grapes': 2, 'watermelon': 7}):
+    if direction == True:
+        if a in d:
+            return d[a]
+        else:
+            return False
+    else:
+        if a not in d:
+            return True
+        else:
+            return d[a]
+
+# Call the function so that it returns False and assign that function call to the variable c_false
+c_false = checkingIfIn('peas')
