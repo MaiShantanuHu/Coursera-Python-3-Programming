fd9041e49cef89089198e4db7d4d692fda5fab87
shantanu <66246470+MaiShantanuHu@users.noreply.github.com>
2020-09-11 23:09:54 +0530

    Add files via upload

---
+++ Python Functions, Files, and Dictionaries/Week-5/Sorting.py
@@ -0,0 +1,19 @@
+'''Q-1. Sort the following string alphabetically, from z to a, and assign it to the variable sorted_letters.'''
+
+letters = "alwnfiwaksuezlaeiajsdl"
+sorted_letters = sorted(letters, reverse = True)
+print(sorted_letters)
+    
+''' Q-2. Sort the list below, animals, into alphabetical order, a-z. Save the new list as animals_sorted.'''
+
+animals = ['elephant', 'cat', 'moose', 'antelope', 'elk', 'rabbit', 'zebra', 'yak', 'salamander', 'deer', 'otter',
+           'minx', 'giraffe', 'goat', 'cow', 'tiger', 'bear']
+animals_sorted = sorted(animals)
+print(animals_sorted)
+
+''' Q-3. The dictionary, medals, shows the medal count for six countries during the Rio Olympics. Sort the country names
+so they appear alphabetically. Save this list to the variable alphabetical.'''
+
+
+
+
